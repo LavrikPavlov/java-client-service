a06de37180e08f91fd487912e15b84858e86a17f
misha <misha-kaza2013@yanadex.ru>
2024-03-07 23:51:05 +0700

    upload project java-client-service

---
+++ src/main/java/ru/kazan/clientservice/JavaClientServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kazan.clientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaClientServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kazan/clientservice/JavaClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kazan.clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
