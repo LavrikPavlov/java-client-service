17af72f39de5ac5dd77fc20646eeb87b72be4470
MikhailKazan <misha-kazan2013@yandex.ru>
2024-03-10 21:27:14 +0700

    Add liquidbase/Add migrations

--- code.sql
+++ code.sql
@@ -2,30 +2,30 @@ CREATE SCHEMA client_storage;
 
 CREATE TABLE Client(
     id uuid primary key,
-    first_name varchar(15),
-    last_name varchar(15),
+    first_name varchar(15) not null ,
+    last_name varchar(15) not null ,
     patronymic varchar(15),
-    mobile_phone varchar(10),
+    mobile_phone varchar(10) not null unique,
     age int check ( age > 0 ),
     date_registration date default CURRENT_DATE,
     status int default 0,
-    passport_id int references Passport(id) unique
+    passport_id bigint references Passport(id) unique
 );
 
 CREATE TABLE Passport(
     id bigserial primary key,
-    serial varchar(5),
-    number int,
-    gender varchar(3),
-    issued_by varchar(15)
+    serial varchar(5) not null ,
+    number varchar(6) not null ,
+    gender varchar(3) not null ,
+    issued_by varchar(15) not null
 );
 
 CREATE TABLE Address(
     id bigserial primary key,
-    country varchar(15),
-    city varchar(20),
-    street varchar(30),
-    house int,
+    country varchar(15) not null ,
+    city varchar(20) not null ,
+    street varchar(30) not null,
+    house int not null ,
     apartment int
 );
 
@@ -35,3 +35,9 @@ CREATE TABLE Address_Client(
 );
 
 CREATE SEQUENCE client_uuid_seq;
+
+DROP TABLE Address;
+DROP TABLE Address_Client;
+DROP TABLE Passport;
+DROP TABLE Client;
+
